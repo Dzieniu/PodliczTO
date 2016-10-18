020fc826552824e4318358016b5da508e30d5b61
dzieniu <dzieniu@gmail.com>
2016-10-18 19:33:34 +0200

    Connect with mysqlDB

--- src/podliczto/Main.java
+++ src/podliczto/Main.java
@@ -1,13 +1,34 @@
 
 package podliczto;
 
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author Konrad
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         
-    }
+        String url = "jdbc:mysql://localhost:3306/test";
+        String username = "root";
+        String password = "apteka13";
+        String name = "";
+        
+        Connection connection = (Connection) DriverManager.getConnection(url, username, password);
+        System.out.println("Database connected!");
+        
+        Statement st = connection.createStatement();
+        ResultSet rs = st.executeQuery("SELECT * FROM osoba");
+        
+        if(rs.next()) {  
+            name = rs.getString("imie");
+        }
+        System.out.println(name);
     
+    }
 }
